a9042f4d77ab0c23c5e3f8994e70ea0325a99a26
ToastyBear <Toast@live.com.au>
2017-08-30 15:49:41 +1000

    Main Character Testing

---
+++ Isomet/Assets/Script/Vacuum.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vacuum : MonoBehaviour {
+
+    public GameObject PullOBJ;
+    public float ForceSpeed = 30f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+
+        if (coll.gameObject.tag == ("Pullable"))
+        {
+            PullOBJ = coll.gameObject;
+
+            PullOBJ.transform.position = Vector3.MoveTowards
+                (PullOBJ.transform.position,
+                 transform.position,
+                 ForceSpeed * Time.deltaTime);
+        }
+
+    }
+}
